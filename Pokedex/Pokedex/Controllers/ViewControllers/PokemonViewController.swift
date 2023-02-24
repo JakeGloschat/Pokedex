18d1acac86981798b6b9818ec50eb9cbd566d482
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-23 17:40:47 -0700

    Finished app

--- Pokedex/Pokedex/Controllers/ViewControllers/PokemonViewController.swift
+++ Pokedex/Pokedex/Controllers/ViewControllers/PokemonViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class PokemonViewController: UIViewController {
-
+    
     @IBOutlet weak var pokemonSearchBar: UISearchBar!
     @IBOutlet weak var pokemonIDLabel: UILabel!
     @IBOutlet weak var pokemonNameLabel: UILabel!
@@ -17,6 +17,59 @@ class PokemonViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pokemonMovesTableView.dataSource = self
+        pokemonMovesTableView.delegate = self
+        pokemonSearchBar.delegate = self
     }
+    
+    // MARK: - Properties
+    var pokemon: Pokemon?
+    
+    // MARK: - Functions
+    
+    func updateViews(pokemon: Pokemon) {
+        PokemonController.fetchImage(forPokemon: pokemon) { image in
+            guard let image = image else { return }
+            DispatchQueue.main.async {
+                self.pokemon = pokemon
+                self.pokemonNameLabel.text = pokemon.name
+                self.pokemonIDLabel.text = "\(pokemon.id)"
+                self.pokemonSpriteImageView.image = image
+                self.pokemonMovesTableView.reloadData()
+            }
+        }
+    }
+    
+    
 }// End
 
+extension PokemonViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let pokemon = pokemon else { return 0}
+        return pokemon.moves.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "moveCell", for: indexPath)
+        let move = pokemon?.moves[indexPath.row]
+        var config = cell.defaultContentConfiguration()
+        config.text = move
+        cell.contentConfiguration = config
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "moves"
+    }
+    
+}
+
+extension PokemonViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        PokemonController.fetchPokemon(searchTerm: searchText) { pokemon in
+            guard let pokemon = pokemon else { return }
+            self.updateViews(pokemon: pokemon)
+        }
+    }
+}
--- Pokedex/Pokedex/ModelController/PokemonController.swift
+++ Pokedex/Pokedex/ModelController/PokemonController.swift
@@ -5,14 +5,14 @@
 //  Created by Jake Gloschat on 2/23/23.
 //
 
-import Foundation
+import UIKit
 
 class PokemonController {
     
     static func fetchPokemon(searchTerm: String, completion: @escaping (Pokemon?) -> Void) {
         guard let baseURL = URL(string: Constants.PokemonURL.baseURL) else { completion(nil) ; return }
         var urlComponent = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
-        urlComponent?.path.append(searchTerm)
+        urlComponent?.path.append(searchTerm.lowercased())
         
         guard let finalURL = urlComponent?.url else { completion (nil) ; return }
         print("FinalURL: \(finalURL)")
@@ -43,4 +43,21 @@ class PokemonController {
             }
         }.resume()
     }
+    
+    static func fetchImage(forPokemon: Pokemon, completion: @escaping (UIImage?) -> Void) {
+        guard let finalURL = URL(string: forPokemon.spritePath) else {completion(nil) ; return }
+       
+        // MARK: - DataTask
+        URLSession.shared.dataTask(with: finalURL) { data, _, error in
+            if let error = error {
+                print("There was an error fetching the data. the url is \(finalURL), the error is \(error.localizedDescription)")
+            }
+            
+            guard let data = data else { completion(nil) ; return }
+            
+                let pokemonImage = UIImage(data: data)
+                completion(pokemonImage)
+        }.resume()
+    }
 }
+
